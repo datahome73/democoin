9a76c6d828571c0fac793a42ec4d19b1746d9e7a
datahome73 <datahome@qq.com>
2019-04-17 17:28:39 +0800

    update

--- x/democoin/codec.go
+++ x/democoin/codec.go
@@ -1,1 +1,11 @@
 package democoin
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+// RegisterCodec registers concrete types on wire codec
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgSetName{}, "democoin/SetName", nil)
+	cdc.RegisterConcrete(MsgBuyName{}, "democoin/BuyName", nil)
+}
